5b86588311c8b5ea7ccd4cdf42ccdac9ca420f95
truong-ngo <truong.ngo1993@gmail.com>
2024-11-27 23:52:54 +0700

    add

--- src/main/java/com/nob/validation/validator/ObjectAttributeValidator.java
+++ src/main/java/com/nob/validation/validator/ObjectAttributeValidator.java
@@ -1,10 +1,12 @@
 package com.nob.validation.validator;
 
+import com.nob.utils.MapUtils;
 import com.nob.validation.constraint.Constraint;
 import com.nob.validation.context.Attribute;
 import com.nob.validation.context.Context;
 import com.nob.validation.validator.constant.ParamKey;
 
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -22,10 +24,15 @@ public class ObjectAttributeValidator implements Validator {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Result validateInternal(Context context, Attribute attribute, Constraint constraint) {
         Object val = attribute.getValue();
         if (Objects.isNull(val)) return Result.valid();
         String profile = constraint.getParam(ParamKey.PROFILE, String.class);
-        return delegate.validate(val, attribute.getJavaType(), profile);
+        Result r = delegate.validate(val, attribute.getJavaType(), profile);
+        if (r.isValid()) return r;
+        Map<String, Object> message = (Map<String, Object>) r.getMessage();
+        message = MapUtils.appendKeyPrefix(message, ".");
+        return Result.invalid(message);
     }
 }
